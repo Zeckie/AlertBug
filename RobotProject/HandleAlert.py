4eb96689750b3f8beff5f3703f4cb9fd92ac5c67
Zeckie <49095968+Zeckie@users.noreply.github.com>
2019-10-28 22:36:23 +1000

    Add exception handling

--- RobotProject/HandleAlert.py
+++ RobotProject/HandleAlert.py
@@ -13,8 +13,20 @@ def alt_handle_alert(timeout:str=None):
     timeout = LibraryComponent(sl).get_timeout(timeout)
     wait = WebDriverWait(sl.driver, timeout)
 
-    alert = wait.until(EC.alert_is_present())
-    alert.dismiss()
+    try:
+        #from AlertKeywords._wait_alert
+        alert = wait.until(EC.alert_is_present())
+        
+        #from AlertKeywords._handle_alert
+        alert.dismiss()
+    except TimeoutException:
+            raise AssertionError('Alert not found in %s.'
+                             % secs_to_timestr(timeout))
+    except WebDriverException as e:
+        raise AssertionError('An exception occurred waiting for alert: %s'
+                             % str(e))
+
+  
 
 
 
